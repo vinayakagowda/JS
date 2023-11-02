8e60e5d70e320df3913a2b7758c2c61e11a1249a
vinayakagowda <85830769+vinayakagowda@users.noreply.github.com>
2023-11-02 14:26:02 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+let a="this is a string"
+let b=10//this is a integer for bigger numbers its better to use bigint variable
+let c= false //this is a boolean, it has two values true and false
+c===false
+let d= null //this is a null variable, null doesnt mean that the variable is empty.
+let e= undefined;//this is an undefined variable
+
+
---
+++ variables.js
@@ -0,0 +1,5 @@
+//var v1="This is a var"
+const v1="This is const"
+console.log(v1)
+let v2="This is a let "
+console.table([v1,v2])
