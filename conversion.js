208dad172f9b1d4589d52f50f3aba70182b9a653
vinayakagowda <85830769+vinayakagowda@users.noreply.github.com>
2023-11-11 11:25:21 +0530

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,8 @@
+let n=10;
+console.log(typeof(n))
+let valueInnumber=Number(n)
+console.log(typeof(valueInnumber))
+let isloggedin = 1;
+let booleanm=Boolean(isloggedin)
+console.log(typeof(booleanm))
+console.log(booleanm)
---
+++ operations.js
@@ -0,0 +1,5 @@
+let value = 6
+let negValue = -value
+console.table([value,negValue])
+console.log(null == 0);
+console.log(null >= 0);
